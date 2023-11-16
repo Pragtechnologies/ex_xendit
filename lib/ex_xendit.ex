32b39e5fbabbc9f68c5e3300d94aeb92f1d8846d
Noel Del Castillo <noel@pragtechnologies.com>
2023-11-16 14:50:33 +0800

    Initial commit

---
+++ lib/ex_xendit.ex
@@ -0,0 +1,18 @@
+defmodule ExXendit do
+  @moduledoc """
+  Documentation for `ExXendit`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExXendit.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_xendit_test.exs
@@ -0,0 +1,8 @@
+defmodule ExXenditTest do
+  use ExUnit.Case
+  doctest ExXendit
+
+  test "greets the world" do
+    assert ExXendit.hello() == :world
+  end
+end
