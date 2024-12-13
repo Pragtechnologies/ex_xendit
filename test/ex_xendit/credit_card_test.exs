be84072b0044ef1f3a023df1720aa98e92cdea2a
Noel Del Castillo <noel@pragtechnologies.com>
2024-12-13 11:48:46 +0800

    Add tests on create charge

--- test/ex_xendit/credit_card_test.exs
+++ test/ex_xendit/credit_card_test.exs
@@ -9,31 +9,76 @@ defmodule ExXendit.CreditCardTest do
     :ok
   end
 
-  describe "create_charge/1 for main account for multi use" do
+  describe "create_charge/1 with capture true for main account" do
     test "will return valid result" do
-      # use_cassette "valid_create_token" do
-      params = %{
-        amount: nil,
-        card_data: %{
-          account_number: "4456530000001096",
-          exp_month: "12",
-          exp_year: "2029",
-          card_holder_first_name: "John",
-          card_holder_last_name: "Doe",
-          card_holder_email: "johndoe@gmai.com",
-          card_holder_phone_number: "628212223242526"
-        },
-        external_id: "test12345",
-        card_cvn: "123",
-        is_multiple_use: true,
-        should_authenticate: true,
-        currency: "PHP",
-        mid_label: nil
-      }
+      use_cassette "valid_create_charge" do
+        params = %{
+          token_id: "675800ee97e3c90016697cf2",
+          authentication_id: "675b9652da69a10016a96628",
+          external_id: "8aa91fed-48d1-425d-a9c3-96fa16934167",
+          amount: 788.60,
+          currency: "PHP",
+          capture: true
+        }
 
-      assert {:ok, %{body: body}} = CreditCard.create_charge(params) |> dbg()
-      # assert %{"business_id" => "6163f373dac5503a0705b783"} = body
-      # end
+        assert {:ok, %{body: body}} = CreditCard.create_charge(params)
+        assert body["approval_code"] == "831000"
+      end
+    end
+  end
+
+  describe "create_charge/1 with capture false for main account" do
+    test "will return valid result" do
+      use_cassette "valid_create_charge_capture_false" do
+        params = %{
+          token_id: "675809b597e3c90016698461",
+          authentication_id: "675b9588da69a10016a9661b",
+          external_id: "9408f310-6dbc-4bc4-8c7a-b28929a231b8",
+          amount: 788.60,
+          currency: "PHP",
+          capture: false
+        }
+
+        assert {:ok, %{body: body}} = CreditCard.create_charge(params)
+        assert body["approval_code"] == "831000"
+      end
+    end
+  end
+
+  describe "create_charge/1 with capture false for sub account" do
+    test "will return valid result" do
+      use_cassette "valid_create_charge_capture_false_sa" do
+        params = %{
+          token_id: "675bac81da69a10016a9698f",
+          authentication_id: "675bac8fda69a10016a96991",
+          external_id: "8640be02-dec2-495c-a5b0-b64f736039e7",
+          amount: 788.60,
+          currency: "PHP",
+          capture: false
+        }
+
+        headers = %{
+          sub_account_id: "656f138e277d4ff66454ecab"
+        }
+
+        assert {:ok, %{body: body}} = CreditCard.create_charge(params, headers)
+        assert body["approval_code"] == "831000"
+      end
+    end
+  end
+
+  describe "get_charge/1 for main account for multi use" do
+    test "will return valid result" do
+      use_cassette("valid_get_charge") do
+        charge_id = "cc_674c881e5619b600163f598a"
+
+        params = %{
+          id_type: "charge"
+        }
+
+        assert {:ok, %{body: body}} = CreditCard.get_charge(charge_id, params)
+        assert body["approval_code"] == "831000"
+      end
     end
   end
 end
